f2ecb38ce4def9abaac0d30d6817f9f0630e2185
Elvirausman <u.zaynab.elvira@gmail.com>
2024-01-20 03:22:53 -0800

    PYTHON DATA STRUCTURE LISTS

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    updated_str = ''
-    for i in my_string:
-        if i != 'c' and i != 'C':
-            updated_str += i
-    return (updated_str)
+    new_s = my_string.translate({ord('c'): None})
+    new_s = new_s.translate({ord('C'): None})
+    return new_s
 
