7c9bf61db52c1d2b7bdf2552bdb956dc7b2ee59d
Elvirausman <u.zaynab.elvira@gmail.com>
2024-02-05 08:02:57 -0800

    PYTHON EXCEPTIONS

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
+    """Print the first x elements of a list that are integers.
 
-    try:
-        for i in range(x):
-            value = my_list[i]
-            if type(value) == int:
-                print("{:d}".format(value), end=" ")
-                count += 1
-    except IndexError:
-        pass  # Catching IndexError when x is greater than the length of my_list
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
 
-    print()  # Print a new line after printing integers
-    return count
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (ret)
--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,7 +1,5 @@
-#1/usr/bin/python3
+#!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    try:
-        raise NameError(message)
-    except NameError as e:
-        print("Caught an exception:", e)
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
