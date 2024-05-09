24fd6b831307e60a3314ef4520d7457627016ece
Elvirasuman <u.zaynab.elvira@gmail.com>
2024-05-09 19:34:37 +0000

    PYTHON NETWORKING'

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,2 +1,3 @@
-ends a JSON POST request to a URL, and displays the body of the response.
-curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+#!/bin/bash
+# script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
+curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
